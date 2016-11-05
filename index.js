ca0020f86fc62e5ceec31c094e9133018d7a82c7
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-11-06 05:41:52 +0800

    Move to array-based rendering

--- index.js
+++ index.js
@@ -3,23 +3,8 @@ const map = require('object-loops/map')
 const values = require('object-loops/values')
 const normalize = require('./lib/normalize')
 const applyStyle = require('./lib/apply_style')
-
-const COLORS = [
-  '#1abc9c',
-  '#2ecc71',
-  '#3498db',
-  '#9b59b6',
-  '#34495e',
-  '#f1c40f',
-  '#e67e22',
-  '#e74c3c'
-]
-
-const SYMBOLS = {
-  male: '♂',
-  female: '♀',
-  deceased: '†'
-}
+const join = require('./lib/join')
+const COLORS = require('./lib/defaults/colors')
 
 function render (data) {
   data = normalize(data)
@@ -28,18 +13,18 @@ function render (data) {
   const people = data.people || {}
   const families = data.families || {}
 
-  return redent(`
-    digraph G {
-      # Header
-      edge [${applyStyle(data, [':edge'])}];
-      node [${applyStyle(data, [':node'])}];
-      ${applyStyle(data, [':digraph'], { sep: '; ' })};
-
-      # People
-      ${values(map(people, (p, id) => renderPerson(p || {}, id, data))).join('\n')}
-      ${values(map(families, (f, id) => renderFamily(f || {}, id, data))).join('')}
-    }
-  `)
+  return join([
+    'digraph G {',
+    '# Header',
+    `edge [${applyStyle(data, [':edge'])}];`,
+    `node [${applyStyle(data, [':node'])}];`,
+    `${applyStyle(data, [':digraph'], { sep: '; ' })};`,
+    '',
+    '# People',
+    values(map(people, (p, id) => renderPerson(p || {}, id, data))),
+    values(map(families, (f, id) => renderFamily(f || {}, id, data))),
+    '}'
+  ], '\n')
 }
 
 function renderPerson (person, id, data) {
@@ -71,38 +56,47 @@ function renderFamily (family, id, data) {
   const hasChildren = (children.length + children.length) > 0
   const hasManyChildren = (children.length + children.length) > 1
 
-  return redent(`
-    ${(hasParents) ? `
-      # Family ${id}
-      m${id} [color="${color}", ${applyStyle(data, [':union'])}];
-      {rank=same; "${parents.join('", "')}"};
-      ${parents.map(parent => {
+  return [
+    hasParents && renderParents(),
+    hasParents && hasChildren && renderLink(),
+    hasChildren && renderKids(),
+    (hasManyChildren > 1) && renderKidLinks()
+  ]
+
+  function renderParents () {
+    return [
+      `# Family ${id}`,
+      `m${id} [color="${color}", ${applyStyle(data, [':union'])}];`,
+      `{rank=same; "${parents.join('", "')}"};`,
+      parents.map(parent => {
         return `"${parent}":e -> m${id} [color="${color}", ${applyStyle(data, [':parent-link'])}];`
-      }).join('\n')}
-      ${parents2.map(parent => {
+      }),
+      parents2.map(parent => {
         return `"${parent}":e -> m${id} [color="${color}", ${applyStyle(data, [':parent-link', ':parent2-link'])}];`
-      }).join('\n')}
-    ` : ''}
+      })
+    ]
+  }
 
-    ${(hasParents && hasChildren) ? `
-      m${id} -> k${id} [color="${color}", weight=10, ${applyStyle(data, [':parent-link'])}];
-    ` : ''}
+  function renderLink () {
+    return `m${id} -> k${id} [color="${color}", weight=10, ${applyStyle(data, [':parent-link'])}];`
+  }
 
-    ${hasChildren ? `
-      k${id} [${applyStyle(data, [':children'])}];
-      {rank=same; "${children.join('", "')}"};
-      ${children.map(kid => {
+  function renderKids () {
+    return [
+      `k${id} [${applyStyle(data, [':children'])}];`,
+      `{rank=same; "${children.join('", "')}"};`,
+      children.map(kid => {
         return `k${id} -> "${kid}":w [weight=2, color="${color}", ${applyStyle(data, [':child-link'])}];`
-      }).join('\n')}
-      ${children2.map(kid => {
+      }),
+      children2.map(kid => {
         return `k${id} -> "${kid}":w [weight=2, color="${color}", ${applyStyle(data, [':child-link', ':child2-link'])}];`
-      }).join('\n')}
-    ` : ''}
+      })
+    ]
+  }
 
-    ${hasManyChildren > 1 ? `
-      {"${children.concat(children2).join('" -> "')}" [style=invis, weight=5]};
-    ` : ''}
-  `)
+  function renderKidLinks () {
+    return `{"${children.concat(children2).join('" -> "')}" [style=invis, weight=5]};`
+  }
 }
 
 /*
